008cf41d07577e7057d34ffbff5ed70c99c3e6b8
rupom <mohrajulrupom@gmail.com>
2023-10-14 23:59:25 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}" , "design_process.html","project-located.html","timeline_for_design.html",
+  "your-role.html","experience-in-design.html","pick-the-description.html"],
+  theme: {
+    fontFamily: {
+      'inter': ['Inter', 'sans-serif'],
+      
+    },
+    extend: {},
+  },
+  plugins: [],
+}
